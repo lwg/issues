12998d47769cf745f92b115e2b1b03f2e11b0efd
AlisdairM <public@alisdairm.net>
2010-11-18 09:01:09 -0500

    Moved many NAD issues from Batavia back to Pending issues until the whole set are reviewed together

--- src/lists.cpp
+++ src/lists.cpp
@@ -15,12 +15,13 @@
 // The only known compiler to support all of this today is the experimental gcc trunk (4.6)
 
 // TODO
+// .  Sort the Revision comments in the same order as the 'Status' reports, rather than alphabetically
 // .  Lots of tidy and cleanup after merging the revision-generating tool
-// .  Create two new index files for the 'tentative' and 'unresolved' documents
-// .  sort-by-last-modified-date should offer some filter or separation to see only the issues modified since  the last meeting
 // .  Refactor more common text
 // .  Split 'format' function and usage to that the issues vector can pass by const-ref in the common cases
 // .  Document the purpose amd contract on each function
+// Waiting on external fix for preserving file-dates
+// .  sort-by-last-modified-date should offer some filter or separation to see only the issues modified since  the last meeting
 
 
 // standard headers
