bda52571fc028ee41d305b5f3e4c41b72130bcb3
Dani-Hub <daniel.kruegler@googlemail.com>
2011-02-16 19:24:26 +0100

    Add new batch file for "build-only" (no check-out), set 2016 to "open"

--- bin/build_lists.bat
+++ bin/build_lists.bat
@@ -1,14 +1,2 @@
 call git pull
-bin\lists
-@echo off
-if errorlevel 1 goto error
-lwg-active.html
-goto done
-
-:error
-echo ***********************************
-echo ********** build failure **********
-echo ***********************************
-
-:done
-
+bin\build_lists_only
---
+++ bin/build_lists_only.bat
@@ -0,0 +1,13 @@
+bin\lists
+@echo off
+if errorlevel 1 goto error
+lwg-active.html
+goto done
+
+:error
+echo ***********************************
+echo ********** build failure **********
+echo ***********************************
+
+:done
+
