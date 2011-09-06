28b540bbb49e5a5d17400c526d298b7b3357107b
AlisdairM <public@alisdairm.net>
2011-09-06 09:41:45 -0400

    List prep for next pre-meeting mailing

--- src/lists.cpp
+++ src/lists.cpp
@@ -978,7 +978,7 @@ auto LwgIssuesXml::get_revisions(std::vector<issue> const & issues, std::string 
 
    r += "<li>";
 //   r += "R74: 2011-02-28 pre-Madrid mailing";   // This is the form we are copying
-   r += "R76: 2011-09-06 post-Bloomington mailing";   // We should date and *timestamp* this reference, as we expect to generate several documents per day
+   r += "D77: 2011-09-06 pre-Kona mailing";   // We should date and *timestamp* this reference, as we expect to generate several documents per day
 //   r += "Madrid meeting resolutions";   // We should date and *timestamp* this reference, as we expect to generate several documents per day
    r += diff_report;
    r += "</li>\n";
